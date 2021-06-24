ab41d76610771b3e2e9c06d871b133efe2ab5c7e
5talin 5 <59964372+5talinFive@users.noreply.github.com>
2021-06-24 17:17:16 -0500

    Add files via upload
    Servidor y npm, restructuración

--- NTP/Modulo_1/programming/01_Projects/index.js
+++ NTP/Modulo_1/programming/01_Projects/index.js
@@ -7,6 +7,31 @@ app.get('/', (req, res) => {
     res.send("Hla mundo con ExpressJs");
 })
 
+app.get("/saludo", (req, res) =>{
+    //Objeto req.query
+    // console.log(req.query);
+    //Obtenemos la query del Objeto request
+    const { 
+        query: { nombre, apellido }, 
+    } = req;
+
+    res.json({
+        saludo: `Hla soy ${nombre} ${apellido}`,
+    });
+});
+
+app.get("/saludo/:nombre", (req, res) => {
+    const { 
+        params: { nombre }, 
+    } = req;
+    console.log(req.params);
+    
+
+    res.json({
+        nombre: nombre,
+    })
+})
+
 app.listen(3000, () => {
     console.log("Servidor escuchando en http://localhost:3000");
-})
+});
---
+++ NTP/Modulo_1/programming/01_Projects/routes/index.js
@@ -0,0 +1,20 @@
+//Traemos el paquete
+const { Router } = require('express')
+
+//Inicializamos router
+const router = Router()
+
+//creacion de los endpoint
+
+//Exportamos las rutas
+module.exports.RouterIndex = router
+
+// module = {
+//     exports: {
+//         RouterIndex: router
+//     }
+// }
+
+
+//CONSULTAR MIDLEWARE
+
