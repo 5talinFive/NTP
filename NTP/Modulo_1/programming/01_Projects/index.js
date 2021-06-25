90c5dd406996dc23f3b4f10fea7492f3cd9f5411
5talin 5 <59964372+5talinFive@users.noreply.github.com>
2021-06-25 18:27:17 -0500

    Add files via upload
    Implementación Routers-modelado

--- NTP/Modulo_1/programming/01_Projects/index.js
+++ NTP/Modulo_1/programming/01_Projects/index.js
@@ -1,36 +1,19 @@
-const express = require('express'); 
+const express = require('express');
 
 //genero una aplicacion express
 const app = express()
-//EndPoint
-app.get('/', (req, res) => {
-    res.send("Hla mundo con ExpressJs");
-})
 
-app.get("/saludo", (req, res) =>{
-    //Objeto req.query
-    // console.log(req.query);
-    //Obtenemos la query del Objeto request
-    const { 
-        query: { nombre, apellido }, 
-    } = req;
+//Routes
+const { RouterIndex } = require('./routes/index');
 
-    res.json({
-        saludo: `Hla soy ${nombre} ${apellido}`,
-    });
-});
-
-app.get("/saludo/:nombre", (req, res) => {
-    const { 
-        params: { nombre }, 
-    } = req;
-    console.log(req.params);
-    
-
-    res.json({
-        nombre: nombre,
-    })
-})
+app.use("/", RouterIndex);
+// app.use("/user", RouterIndex);
+// app.use("user/data", RouterIndex);
+/**
+        "/" + "/" + "//" => "/"
+        "/" + "saludo" => "//saludo" => "/saludo"
+        "/" + "/saludo/:nombre" = "//saludo/:nombre => "/saludo:nombre"
+ */
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en http://localhost:3000");
--- NTP/Modulo_1/programming/01_Projects/routes/index.js
+++ NTP/Modulo_1/programming/01_Projects/routes/index.js
@@ -5,16 +5,38 @@ const { Router } = require('express')
 const router = Router()
 
 //creacion de los endpoint
+router
+    .get("/", (req, res) => {
+        res.send("Hola mundo con ExpressJs");
+    })
+    .get("/saludo", () => {
+        const { query } = req;
+        res.json({
+            saludo: `Hola soy ${query.nombre} ${query.apellido}`,
+        });
+    })
+        .get("/saludo/:nombre", (req, res) => {
+            const { params } = req;
+            res.json({
+                nombre: params.nombre,
+            });
+        });
+        
+        //Exportamos las rutas
+        module.exports.RouterIndex = router
+        
+        // module = {
+        //     exports: {
+        //         RouterIndex: router
+        //     }
+        // }
+        
+        
+        //CONSULTAR MIDLEWARE
+    
 
-//Exportamos las rutas
-module.exports.RouterIndex = router
-
-// module = {
-//     exports: {
-//         RouterIndex: router
-//     }
-// }
+        
+        
 
 
-//CONSULTAR MIDLEWARE
 
